78118a84fb8ebea83cf5341644b9c33a4530d3ca
Sam <sam.camhi@gmail.com>
2023-04-15 11:58:14 -0400

    Tasl Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+let form = document.querySelector("#create-task-form")
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newListItem = document.createElement("li");
+  const btn = document.createElement("button");
+  btn.addEventListener("click", (e) => e.target.parentNode.remove());
+  newListItem.textContent = `${e.target.new_task_description.value} `;
+  btn.textContent = "X";
+  newListItem.appendChild(btn);
+  document.querySelector("#tasks").appendChild(newListItem);
+  form.reset();
+})
+
+
+
 });
